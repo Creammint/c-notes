8860534629b834e4f9ddcf9a7c352b3f0a69c866
Creammint <155428188+Creammint@users.noreply.github.com>
2025-08-12 16:39:48 +0800

    Deploying to gh-pages from @ Creammint/c-notes@4d55e16157df5f9eb3d2bb68cc25a764e39f9da2 🚀

---
+++ assets/notes_blog_CoverMagic专业的封面设计工具.md.Bdvrk2PE.js
@@ -0,0 +1,17 @@
+import{_ as s,c as n,o as e,al as p}from"./chunks/framework.CI6BdnOQ.js";const u=JSON.parse('{"title":"CoverMagic 专业的封面设计工具","description":"","frontmatter":{},"headers":[],"relativePath":"notes/blog/CoverMagic专业的封面设计工具.md","filePath":"notes/blog/CoverMagic专业的封面设计工具.md","lastUpdated":1754987919000}'),l={name:"notes/blog/CoverMagic专业的封面设计工具.md"};function r(i,a,c,t,o,b){return e(),n("div",null,a[0]||(a[0]=[p(`<h1 id="covermagic-专业的封面设计工具" tabindex="-1">CoverMagic 专业的封面设计工具 <a class="header-anchor" href="#covermagic-专业的封面设计工具" aria-label="Permalink to &quot;CoverMagic 专业的封面设计工具&quot;">​</a></h1><p>教程：<a href="https://baiwumm.com/archives/QiSgl5d8" target="_blank" rel="noreferrer">Cover Magic：专业的封面设计工具，支持实时预览和高质量导出</a></p><h3 id="🛠️-本地开发" tabindex="-1">🛠️ 本地开发 <a class="header-anchor" href="#🛠️-本地开发" aria-label="Permalink to &quot;🛠️ 本地开发&quot;">​</a></h3><div class="language- vp-adaptive-theme line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span># 1. 克隆项目</span></span>
+<span class="line"><span>git clone git@github.com:baiwumm/cover-magic.git</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span># 2. 进入项目目录</span></span>
+<span class="line"><span>cd cover-magic</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span># 3. 安装依赖</span></span>
+<span class="line"><span>pnpm install</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span># 4. 启动开发服务器</span></span>
+<span class="line"><span>pnpm dev</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span># 5. 打开浏览器访问</span></span>
+<span class="line"><span># http://localhost:5173</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span># 本机地址</span></span>
+<span class="line"><span># D:\\software\\cover-magic</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br></div></div>`,4)]))}const d=s(l,[["render",r]]);export{u as __pageData,d as default};
